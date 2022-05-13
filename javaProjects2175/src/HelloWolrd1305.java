e4329d3166808a23a0498943b504fd73f3fe74fe
Deepak <Deepak.kumar7v@gmail.com>
2022-05-13 15:14:54 -0700

    Firstlecture JAVA

---
+++ javaProjects2175/src/HelloWolrd1305.java
@@ -0,0 +1,14 @@
+public class HelloWolrd1305 {
+    public static void main(String...s)
+    {
+        int anInt =0;
+        String aString;
+        double aDouble = 3.65;
+        final int DOZEN=12;
+        aString="";
+        System.out.println("an int="+anInt);
+        System.out.println("an int="+aDouble);
+        System.out.println("an int="+DOZEN);
+        System.out.println("an int="+aString);
+    }
+}
